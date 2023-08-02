2a611133feff4fef0182b308ab81985b2a7043ff
DmytroTsaryk79 <dmitrytsarik@gmail.com>
2023-08-02 18:12:18 +0300

    lesson6

---
+++ src/main/java/lessons/lesson6/Main.java
@@ -0,0 +1,23 @@
+package lessons.lesson6;
+
+public class Main {
+    public static void main(String[] args) {
+        String inputString = "Hello Hillel school!";
+        char targetSymbol = 'h';
+
+        int occurrences = findSymbolOccurance(inputString, targetSymbol);
+        System.out.println("Символ " + targetSymbol + " зустрічається " + occurrences + " раз у рядку");
+    }
+
+    public static int findSymbolOccurance(String input, char targetSymbol) {
+        int count = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == targetSymbol) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
