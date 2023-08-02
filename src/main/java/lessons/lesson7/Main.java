24995f81aa0f5c3a70732419916dd762b6c66257
DmytroTsaryk79 <dmitrytsarik@gmail.com>
2023-08-02 19:06:47 +0300

    lesson7

---
+++ src/main/java/lessons/lesson7/Main.java
@@ -0,0 +1,20 @@
+package lessons.lesson7;
+
+public class Main {
+    public static void main(String[] args) {
+        String source = "Hello Hillel school!";
+        String target = "lel";
+
+        int position = findWordPosition(source, target);
+        if (position != -1) {
+            System.out.println("Слово '" + target + "' знаходиться на позиції " + position + " у рядку.");
+        } else {
+            System.out.println("Слово '" + target + "' не знайдено у рядку.");
+        }
+    }
+
+    public static int findWordPosition(String source, String target) {
+        int position = source.indexOf(target);
+        return position;
+    }
+}
